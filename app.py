3a33ad0843271efd603a4eff6a601a9621e4498c
carlos <carlos.wesley.jesus@gmail.com>
2021-07-23 21:55:18 -0300

    staged

---
+++ app.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template, redirect, request 
+from flask_mail import Mail, Message 
+
+
+app = Flask(__name__)
+
+mail_settings = {
+    "MAIL_SERVER": 'smtp.gmail.com',
+    "MAIL_PORT": 465,
+    "MAIL_USE_TLS": False,
+    "MAIL_USE_SSL": True,
+    "MAIL_USERNAME": 'email',
+    "MAIL_PASSWORD": 'senha'
+}
+
+app.config.update(mail_settings) 
+mail = Mail(app) 
+
+
+class Contato: #classe que constrói os atributos do form
+   def __init__ (self, nome, email, mensagem):
+      self.nome = nome
+      self.email = email
+      self.mensagem = mensagem
+
+@app.route('/') #rota da página principal
+def index():
+   return render_template('index.html')
+
+@app.route('/send', methods=['GET', 'POST'])
+def send():
+   if request.method == 'POST':
+      formContato = Contato(
+         request.form['nome'],
+         request.form['email'],
+         request.form['mensagem']
+      )
+
+      msg = Message(
+         subject= 'Contato do seu Portfólio', 
+         sender=app.config.get("MAIL_USERNAME"), 
+         recipients=[app.config.get("MAIL_USERNAME")], 
+         body=f'''O {formContato.nome} com o email {formContato.email}, te mandou a seguinte mensagem: 
+         
+               {formContato.mensagem}''' 
+         )
+      
+      mail.send(msg) 
+   return render_template('send.html', formContato=formContato) 
+
+if __name__ == '__main__':
+   app.run(debug=True)
